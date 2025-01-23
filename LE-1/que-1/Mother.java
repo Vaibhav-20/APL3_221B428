8421bd19b5c8e20a17bf17c55c51a2247221a66a
221B428 <221B428@juet.co.in>
2025-01-23 14:23:57 +0530

    LE-1

---
+++ LE-1/que-1/Mother.java
@@ -0,0 +1,11 @@
+    class Mother
+    {
+        
+    int x;
+
+    void show() {
+        System.out.println("This is the show method of Mother class.");
+    }
+}
+
+
---
+++ LE-1/que-4/Main.java
@@ -0,0 +1,7 @@
+
+class Main {
+    public static void main(String[] args) {
+        One o = new One(10);
+        Two t = new Two();
+    }
+}
